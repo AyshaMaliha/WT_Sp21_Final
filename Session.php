4115cce3f3c3af8a74a164c86a4db82137d24c0e
Aysha Maliha <ayshamaliha15@gmail.com>
2021-03-22 14:16:23 +0600

    Session Update

--- Session.php
+++ Session.php
@@ -6,7 +6,9 @@
 	   $uname=$_POST["uname"];
 	   $pass=$_POST["pass"];
 	   if($uname == "Aysha" && $pass == "123456"){
-		   header("Location: dashboard.php");
+		   session_start();
+			$_SESSION["user"]=$uname;
+			header("Location: Dashboard.php");
 	   }
    }
 
@@ -14,12 +16,15 @@
 <html>
     <head></head>
 	<body>
-		<h1>Welcome<?php echo $SESSION["user"];?></h1>
 	     <form method="post">
 			<input type="text" name="uname" placeholder="username"><br>
 			<input type="password" name="pass" placeholder="password"><br>
 			<input type="submit" value="Log In"><br>
 		</form>
+		
+<?php
+	session_destroy();
+?>		
 	</body>		
 
 
